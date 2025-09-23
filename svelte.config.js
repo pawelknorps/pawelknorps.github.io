ada0179764dc2a885267d9974ecd6bd40183cb57
pawelknorps <56291219+pawelknorps@users.noreply.github.com>
2025-09-23 16:04:58 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
+		// Replace adapter-auto with the static adapter.
 		adapter: adapter({
+			// The fallback option is useful for Single-Page Applications (SPAs)
+			// to handle client-side routing on a static host.
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 

498540f5286f440e816903a06619f928a55b2fa9
pawelknorps <56291219+pawelknorps@users.noreply.github.com>
2025-09-23 16:40:16 +0200

    feat: Configure SvelteKit for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static'; // <-- Change this line
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// Replace adapter-auto with the static adapter.
-		adapter: adapter({
-			// The fallback option is useful for Single-Page Applications (SPAs)
-			// to handle client-side routing on a static host.
-			fallback: '404.html'
-		})
+		adapter: adapter()
 	}
 };
 

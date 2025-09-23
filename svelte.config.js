2e74246233cbcd241e01a9345987287d8dcad467
pawelknorps <56291219+pawelknorps@users.noreply.github.com>
2025-09-23 15:57:39 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: null
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "pawelknorps.github.io" : "", 
+			base: process.env.NODE_ENV === "production" ? "" : "", 
 		},
 	},
 };
